9b059a3a131d7aada3eaa035790463d7889bbb93
Seanong011 <keifferong01@gmail.com>
2026-03-30 15:21:48 +0800

    update: FrontRow and ToRow instances to varivables

--- haskell-opac/src/Models/Book.hs
+++ haskell-opac/src/Models/Book.hs
@@ -1,6 +1,9 @@
 module Models.Book where
 
 import Data.Text (Text)
+import Database.SQLite.Simple (FromRow(..), field)
+import Database.SQLite.Simple.ToRow (ToRow(..))
+import Database.SQLite.Simple.ToField (toField)
 
 data Book = Book
   { bookID :: Int,
@@ -13,3 +16,25 @@ data Book = Book
     bookTotalCopies :: Int
   }
   deriving (Show, Eq)
+
+instance FromRow Book where
+  fromRow = Book
+    <$> field  -- book_id
+    <*> field  -- title
+    <*> field  -- author
+    <*> field  -- publication
+    <*> field  -- isbn
+    <*> field  -- genre
+    <*> field  -- available_copies
+    <*> field  -- total_copies
+
+instance ToRow Book where
+  toRow b =
+    [ toField (bookTitle           b)
+    , toField (bookAuthor          b)
+    , toField (bookPublication     b)
+    , toField (bookISBN            b)
+    , toField (bookGenre           b)
+    , toField (bookAvailableCopies b)
+    , toField (bookTotalCopies     b)
+    ]
