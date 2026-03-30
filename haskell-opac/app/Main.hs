6ed833cd14e97fa95c3724b296b35e78db9b3f16
Seanong011 <keifferong01@gmail.com>
2026-03-30 15:26:57 +0800

    update: app routing

--- haskell-opac/app/Main.hs
+++ haskell-opac/app/Main.hs
@@ -1,3 +1,16 @@
+module Main where
 
+import Control.Monad.IO.Class (liftIO)
+import DB.Connection (withConnection)
+import DB.Migration (runMigrations)
+import App.Env (Env(..), AppM, runAppM)
+import CLI.Repl (startRepl)
 
-
+main :: IO ()
+main = withConnection "opac.db" $ \conn -> do
+  runMigrations conn
+  let env = Env
+        { envConnection = conn
+        , envDBPath     = "opac.db"
+        }
+  runAppM env startRepl
